09d149c16622b090f001fdc7c872c816681841d4
SERGIO-GONZÁLEZ-JIMÉNEZ <166758708+ssergioogj@users.noreply.github.com>
2026-03-04 10:44:15 +0100

    continuación EDA

---
+++ visualizer.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import streamlit as st
+
+import pickle as pkl
+
+file = "./dataset/bank_10.pkl"
+
+with open(file, 'rb') as fd:
+    df = pkl.load(fd)
+    print(df)
+
+# Extract the number of variables and instances
+df_shape = df.shape
+
+# Create a chart
+st.write("Número de instancias y variables")
+st.table(df_shape, columns=["Instancias", "Variables"])
